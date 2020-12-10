b7a61f8d4cbddabe7b71973642b52c0a7928139d
shahjaidev <js5161@columbia.edu>
2020-12-09 19:44:21 -0500

    latest gpt3_functions

--- Backend/gpt3_functions_latest.py
+++ Backend/gpt3_functions_latest.py
@@ -6,7 +6,17 @@
 import openai
 
 
-orig_prompt= "Here is an award winning short story:"+ "\n"+ "A woman paddling on a surfboard. Maria sat on her surfboard, as it bobbed up and down on the coastal waves. The neon lights of Ocean Drive burned on the shore in the distance, like green and pink embers. It was just her, the water, and whatever starlight got past the Miami light pollution. A shark swimming in the ocean. She was paddling back to shore when she saw a shark fin in the water. At first she thought it was a manatee or a dolphin. She’d occasionally seen those swimming around here, spent countless hours watching them under the night sky. But this was different. A boat in the lake. A bright spotlight shown down on Maria. Maria looked up, and saw a small fishing boat, floating half-way between her and the shoreline. A stout man in an orange rain coat and fishing boots stood on the deck. “Get out of the water!” A gruff voice shouted. Maria's eyes went wide. She turned and saw the large dorsal fin rapidly advancing. The shark’s body hovered just under the surface like a dark cloud. Maria’s hands dove into the water, paddling faster than she’d ever paddled in her life. Her legs kicked frantically. "
+#orig_prompt= "Here is an award winning short story:"+ "\n"+ "A woman paddling on a surfboard. Maria sat on her surfboard, as it bobbed up and down on the coastal waves. The neon lights of Ocean Drive burned on the shore in the distance, like green and pink embers. It was just her, the water, and whatever starlight got past the Miami light pollution. A shark swimming in the ocean. She was paddling back to shore when she saw a shark fin in the water. At first she thought it was a manatee or a dolphin. She’d occasionally seen those swimming around here, spent countless hours watching them under the night sky. But this was different. A boat in the lake. A bright spotlight shown down on Maria. Maria looked up, and saw a small fishing boat, floating half-way between her and the shoreline. A stout man in an orange rain coat and fishing boots stood on the deck. “Get out of the water!” A gruff voice shouted. Maria's eyes went wide. She turned and saw the large dorsal fin rapidly advancing. The shark’s body hovered just under the surface like a dark cloud. Maria’s hands dove into the water, paddling faster than she’d ever paddled in her life. Her legs kicked frantically. "
+
+prompt_file_path= './GPT3Prompts/origin_prompt.txt'
+
+prompt_file= open(prompt_file_path,'r')
+
+orig_prompt= prompt_file.read()
+
+#print(orig_prompt)
+
+
 story_prompt_file_path= './GPT3Prompts/story_so_far.txt'
 
 openai.api_key = "sk-XHaEg50XHxqsa7j3aSUx7YQNGsHTgmRWjNmRXh3k" 
@@ -15,16 +25,21 @@ openai.api_key = "sk-XHaEg50XHxqsa7j3aSUx7YQNGsHTgmRWjNmRXh3k"
 
 
 def new_story_with_caption(caption, temperature = 0.73):
+
     story_prompt_file= open(story_prompt_file_path, "w+") 
 
     constructed_prompt=  "Here is an award winning short story:"+ "\n"+ caption
     pass_prompt = orig_prompt +"\n\n\n" + constructed_prompt 
     
-    response = openai.Completion.create(engine="davinci", prompt=pass_prompt, max_tokens=150, stop=['\n\n\n'])
+    response = openai.Completion.create(engine="davinci", prompt=pass_prompt, max_tokens=150, stop=['\n','\n\n','\n\n\n'])
     
     reply = response["choices"][0]["text"]  
     print(reply)
     
+    constructed_prompt=  "Here is an award winning short story:"+ "\n"
+    story_prompt_file.write(constructed_prompt)
+
+
     story_prompt_file.write(reply)
 
 
@@ -41,7 +56,7 @@ def continue_story_with_caption(caption, temperature = 0.73):
     constructed_prompt=  story_so_far+ " "+ caption + "."
     pass_prompt = orig_prompt +"\n\n\n" + constructed_prompt 
     
-    response = openai.Completion.create(engine="davinci", prompt=pass_prompt, max_tokens=150, stop=['\n\n\n'])
+    response = openai.Completion.create(engine="davinci", prompt=pass_prompt, max_tokens=150, stop=['\n','\n\n','\n\n\n'])
     
     reply = response["choices"][0]["text"]  
     print(reply)
@@ -63,7 +78,7 @@ def continue_story_without_caption(temperature = 0.73):
 
     pass_prompt = orig_prompt +"\n\n\n" + story_so_far 
     
-    response = openai.Completion.create(engine="davinci", prompt=pass_prompt, max_tokens=150, stop=['\n\n\n'])
+    response = openai.Completion.create(engine="davinci", prompt=pass_prompt, max_tokens=150, stop=['\n','\n\n','\n\n\n'])
     
     reply = response["choices"][0]["text"]  
     print(reply)
@@ -83,7 +98,7 @@ def continue_story_with_text(user_text, temperature = 0.73):
     return continue_story_with_caption(user_text)
 
 
-caption= "Traffic on a city road."
+caption= "a large plane."
 #continue_story_without_caption()
 
 
