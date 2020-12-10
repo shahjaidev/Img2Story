2acfaa6e35010b3397e3f64c421aefa30c0bc733
aishwaryaa-v <57423063+aishwaryaa-v@users.noreply.github.com>
2020-12-09 19:14:40 -0500

    updates to same file

--- gpt3_functions.py
+++ gpt3_functions.py
@@ -1,40 +1,31 @@
 
 #REQUIREMENT: pip install openai
-
 import openai
 import os
 from tools import get_story_prompt_current_file,get_saved_story_new_file,get_new_story_prompt_file,create_file
 
-
+openai.api_key='sk-XHaEg50XHxqsa7j3aSUx7YQNGsHTgmRWjNmRXh3k'
 prompt_file_path= './GPT3Prompts/origin_prompt.txt'
-
-#prompt_file_path= './GPT3Prompts/origin_prompt.txt'
-story_prompt_file_path= get_story_prompt_current_file()
+story_prompt_file_path= './GPT3Prompts/story_so_far.txt'
+#get_story_prompt_current_file()
+#print("story_prompt_file_path", story_prompt_file_path)
 #'./GPT3Prompts/story_so_far.txt'
-display_story_file_path= get_saved_story_new_file()
-#'./SavedStory/saved_story.txt'
+display_story_file_path= './SavedStories/saved_story.txt'
+#get_saved_story_new_file()
 
-openai.api_key = "org-KCAwDUnNQR2Qw3mqomhiDoXF"
-
-
-
-
-#prompt_file= open(prompt_file_path,'r')
-#story_prompt_file= open(story_prompt_file_path, "a+")
-#display_story_file=open(display_story_file_path, "a+")
 
 
 def new_story_with_caption(caption, temperature = 0.84):
-    create_file(story_prompt_file_path,display_story_file_path)
+    #create_file(story_prompt_file_path,display_story_file_path)
     prompt_file=open(prompt_file_path,'r')
-    story_prompt_file=open(story_prompt_file_path, "a+")
-    display_story_file=open(display_story_file_path, "a+")
+    story_prompt_file=open(story_prompt_file_path, "w+")
+    display_story_file=open(display_story_file_path, "w+")
     file_path = story_prompt_file_path
-    filesize = os.stat(file_path).st_size
-    if filesize !=0:
-        story_prompt_new_file_path= get_new_story_prompt_file(story_prompt_file_path)
-        story_prompt_file= open(story_prompt_new_file_path, "a+")
-        file_path = story_prompt_new_file_path
+    # filesize = os.stat(file_path).st_size
+#     if filesize !=0:
+#         story_prompt_new_file_path= get_new_story_prompt_file(story_prompt_file_path)
+#         story_prompt_file= open(story_prompt_new_file_path, "a+")
+#         file_path = story_prompt_new_file_path
 
     original_prompt = prompt_file.read()
     constructed_prompt= "START: "+ caption
@@ -58,7 +49,7 @@ def new_story_with_caption(caption, temperature = 0.84):
 
 #When user chooses to continue the story through uploading an image
 def continue_story_with_caption(caption, temperature = 0.84):
-    create_file(story_prompt_file_path,display_story_file_path)
+    #create_file(story_prompt_file_path,display_story_file_path)
     prompt_file=open(prompt_file_path,'r')
     story_prompt_file=open(story_prompt_file_path, "a+")
     display_story_file=open(display_story_file_path, "a+")
@@ -91,7 +82,7 @@ def continue_story_with_caption(caption, temperature = 0.84):
 
 #User chooses to let the story continue on its own by GPT-3
 def continue_story_without_caption(temperature = 0.84):
-    create_file(story_prompt_file_path,display_story_file_path)
+    #create_file(story_prompt_file_path,display_story_file_path)
     prompt_file=open(prompt_file_path,'r')
     story_prompt_file=open(story_prompt_file_path, "a+")
     display_story_file=open(display_story_file_path, "a+")
@@ -122,7 +113,7 @@ def continue_story_without_caption(temperature = 0.84):
 
 #User chooses to pass some text as input for continuing the story.
 def continue_story_with_text(user_text, temperature = 0.84):
-    create_file(story_prompt_file_path,display_story_file_path)
+    #create_file(story_prompt_file_path,display_story_file_path)
     prompt_file=open(prompt_file_path,'r')
     story_prompt_file=open(story_prompt_file_path, "a+")
     display_story_file=open(display_story_file_path, "a+")
@@ -150,10 +141,3 @@ def continue_story_with_text(user_text, temperature = 0.84):
     display_story_file.close()
 
     return reply
-
-
-caption= "A man standing in the sand with a dog"
-
-new_story_with_caption(caption)
-
-
